3d6d2d332623d725ac94233226e78cb284300b37
dreamer1995 <dreamer199506@126.com>
2023-08-02 05:53:46 +0800

    Fix name error V2

---
+++ DiveBomberEngine/DiveBomberCore/Source/Graphics/DX/CommandList.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "..\GraphicsHeader.h"
+
+#include <memory>
+
+namespace DiveBomber::DEGraphics
+{
+	class Graphics;
+}
+
+namespace DiveBomber::Component
+{
+	class UploadBuffer;
+}
+
+namespace DiveBomber::DX
+{
+	class CommandList final
+	{
+	public:
+		CommandList(wrl::ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE inputType);
+		void Reset();
+		[[nodiscard]] wrl::ComPtr<ID3D12GraphicsCommandList2> GetGraphicsCommandList() const;
+		void Close();
+		[[nodiscard]] D3D12_COMMAND_LIST_TYPE GetCommandListType() const noexcept;
+	private:
+		D3D12_COMMAND_LIST_TYPE type;
+		wrl::ComPtr<ID3D12CommandAllocator> commandAllocator;
+		wrl::ComPtr<ID3D12GraphicsCommandList2> commandList;
+		std::shared_ptr<Component::UploadBuffer> uploadBuffer;
+	};
+}
+
