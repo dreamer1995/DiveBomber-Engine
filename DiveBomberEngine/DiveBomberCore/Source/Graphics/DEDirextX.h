df5d004c35e1ddfb6f24df6c96609ce4ad42ca5f
dreamer1995 <dreamer199506@126.com>
2024-01-20 01:52:05 +0800

    Fix warning.

--- DiveBomberEngine/DiveBomberCore/Source/Graphics/DEDirextX.h
+++ DiveBomberEngine/DiveBomberCore/Source/Graphics/DEDirextX.h
@@ -4,6 +4,7 @@
 #pragma warning(disable:26451)
 #pragma warning(disable:26819)
 #pragma warning(disable:26433)
+#pragma warning(disable:26827)
 #include <d3d12.h>
 #include <d3dx12.h>
 #pragma warning(default:26827)
@@ -11,3 +12,4 @@
 #pragma warning(default:26451)
 #pragma warning(default:26819)
 #pragma warning(default:26433)
+#pragma warning(default:26827)
